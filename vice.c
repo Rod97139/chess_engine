b0f931a4c484ba103b55b714c02a2079729ae58e
Rod97139 <fayaflame@gmail.com>
2024-04-28 12:17:37 +0200

    debug

--- vice.c
+++ vice.c
@@ -5,7 +5,7 @@ int main() {
 
 	AllInit();
 
-	int index = 0;
+	/*int index = 0;
 
 	for (index = 0; index < BRD_SQ_NUM; ++index) {
 		if (index % 10 == 0) printf("\n");
@@ -18,7 +18,7 @@ int main() {
 	for (index = 0; index < 64; ++index) {
 		if (index % 8 == 0) printf("\n");
 		printf("%5d", Sq64ToSq120[index]);
-	}
+	}*/
 
 	return 0;
 } 
