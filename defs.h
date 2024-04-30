626b4259f54b5f43bc912ad4fa80c59ac181ab13
Rod97139 <fayaflame@gmail.com>
2024-04-30 05:43:46 +0200

    game move

--- defs.h
+++ defs.h
@@ -54,6 +54,13 @@ enum { WKCA = 1, WQCA = 2, BKCA = 4, BQCA = 8 };
 typedef struct {
 
 	int move;
+	int score;
+
+} S_MOVE;
+
+typedef struct {
+
+	int move;
 	int castlePerm;
 	int enPas;
 	int fiftyMove;
@@ -94,6 +101,32 @@ typedef struct {
 	// pList[wN][1] = D4;.. .. ..
 
 } S_BOARD;
+
+/* GAME MOVE */
+// 0000 0000 0000 0000 0001 1111 -> 1F
+// 0000 0000 0000 1111 0000 0000 -> F00
+
+// 0000 0000 0000 0000 0000 0111 1111 -> From 0x7F
+// 0000 0000 0000 0011 1111 1000 0000 -> To >> 7, 0x7F
+// 0000 0000 0011 1100 0000 0000 0000 -> Captured >> 14, 0xF
+// 0000 0000 0100 0000 0000 0000 0000 -> EP 0x40000
+// 0000 0000 1000 0000 0000 0000 0000 -> Pawns Start 0x80000
+// 0000 1111 0000 0000 0000 0000 0000 -> Promoted Piece >> 20, 0xF
+// 0001 0000 0000 0000 0000 0000 0000 -> Castle 0x1000000
+
+#define FROMSQ(m) ((m) & 0x7F)
+#define TOSQ(m) (((m) >> 7) & 0x7F)
+#define CAPTURED(m) (((m) >> 14) & 0xF)
+#define PROMOTED(m) (((m) >> 20) & 0xF)
+
+#define MFLAGEP 0x40000
+#define MFLAGPS 0x80000
+#define MFLAGCA 0x1000000
+
+#define MFLAGCAP 0x7C000
+#define MFLAGPROM 0xF00000
+
+
 
 /* MACROS */
 
