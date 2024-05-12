da1a5b5d3893e95549367524743262b18d206857
Rod97139 <fayaflame@gmail.com>
2024-05-12 16:28:57 +0200

    evaluate.c

--- evaluate.c
+++ evaluate.c
@@ -3,24 +3,128 @@
 #include "stdio.h"
 #include "defs.h"
 
+const int PawnTable[64] = {
+    0   ,   0   ,   0   ,   0   ,   0   ,   0   ,   0   ,   0   ,
+    10  ,   10  ,   0   ,  -10  ,  -10  ,   0   ,   10  ,   10  ,
+    5   ,   0   ,   0   ,   5   ,   5   ,   0   ,   0   ,   5   ,
+    0   ,   0   ,   10  ,   20  ,   20  ,   10  ,   0   ,   0   ,
+    5   ,   5   ,   5   ,   10  ,   10  ,   5   ,   5   ,   5   ,
+    10  ,   10  ,   10  ,   20  ,   20  ,   10  ,   10  ,   10  ,
+    20  ,   20  ,   20  ,   30  ,   30  ,   20  ,   20  ,   20  ,
+    0   ,   0   ,   0   ,   0   ,   0   ,   0   ,   0   ,   0
+};
+
+const int KnightTable[64] = {
+    0   ,   -10 ,   0   ,   0   ,   0   ,   0   ,   -10 ,   0   ,
+    0   ,   0   ,   0   ,   5   ,   5   ,   0   ,   0   ,   0   ,
+    0   ,   0   ,   10  ,   10  ,   10  ,   10  ,   0   ,   0   ,
+    0   ,   0   ,   10  ,   20  ,   20  ,   10  ,   5   ,   0   ,
+    5   ,   10  ,   15  ,   20  ,   20  ,   15  ,   10  ,   5   ,
+    5   ,   10  ,   10  ,   20  ,   20  ,   10  ,   10  ,   5   ,
+    0   ,   0   ,   5   ,   10  ,   10  ,   5   ,   0   ,   0   ,
+    0   ,   0   ,   0   ,   0   ,   0   ,   0   ,   0   ,   0
+};
+
+const int BishopTable[64] = {
+    0   ,   0   ,   -10 ,   0   ,   0   ,   -10 ,   0   ,   0   ,
+    0   ,   0   ,   0   ,   10  ,   10  ,   0   ,   0   ,   0   ,
+    0   ,   0   ,   10  ,   15  ,   15  ,   10  ,   0   ,   0   ,
+    0   ,   10  ,   15  ,   20  ,   20  ,   15  ,   10  ,   0   ,
+    0   ,   10  ,   15  ,   20  ,   20  ,   15  ,   10  ,   0   ,
+    0   ,   0   ,   10  ,   15  ,   15  ,   10  ,   0   ,   0   ,
+    0   ,   0   ,   0   ,   10  ,   10  ,   0   ,   0   ,   0   ,
+    0   ,   0   ,   0   ,   0   ,   0   ,   0   ,   0   ,   0
+};
+
+const int RookTable[64] = {
+    0   ,   0   ,   5   ,   10  ,   10  ,   5   ,   0   ,   0   ,
+    0   ,   0   ,   5   ,   10  ,   10  ,   5   ,   0   ,   0   ,
+    0   ,   0   ,   5   ,   10  ,   10  ,   5   ,   0   ,   0   ,
+    0   ,   0   ,   5   ,   10  ,   10  ,   5   ,   0   ,   0   ,
+    0   ,   0   ,   5   ,   10  ,   10  ,   5   ,   0   ,   0   ,
+    0   ,   0   ,   5   ,   10  ,   10  ,   5   ,   0   ,   0   ,
+    25  ,   25  ,   25  ,   25  ,   25  ,   25  ,   25  ,   25  ,
+    0   ,   0   ,   5   ,   10  ,   10  ,   5   ,   0   ,   0
+};
+
+const int Mirror64[64] = {
+    56  ,   57  ,   58  ,   59  ,   60  ,   61  ,   62  ,   63  ,
+    48  ,   49  ,   50  ,   51  ,   52  ,   53  ,   54  ,   55  ,
+    40  ,   41  ,   42  ,   43  ,   44  ,   45  ,   46  ,   47  ,
+    32  ,   33  ,   34  ,   35  ,   36  ,   37  ,   38  ,   39  ,
+    24  ,   25  ,   26  ,   27  ,   28  ,   29  ,   30  ,   31  ,
+    16  ,   17  ,   18  ,   19  ,   20  ,   21  ,   22  ,   23  ,
+    8   ,   9   ,   10  ,   11  ,   12  ,   13  ,   14  ,   15  ,
+    0   ,   1   ,   2   ,   3   ,   4   ,   5   ,   6   ,   7
+};
+
+#define MIRROR64(sq) (Mirror64[(sq)])
+
 int EvalPosition(const S_BOARD *pos) {
 
-    // int pce;
-    // int sq;
-    // int score = pos->material[WHITE] - pos->material[BLACK];
+    int pce;
+    int sq;
+    int score = pos->material[WHITE] - pos->material[BLACK];
 
-    // for(sq = 0; sq < BRD_SQ_NUM; ++sq) {
-    //     pce = pos->pieces[sq];
-    //     if(pce!=OFFBOARD && pce!=EMPTY) {
-    //         if(PieceCol[pce] == WHITE) {
-    //             score += PieceVal[pce];
-    //         } else {
-    //             score -= PieceVal[pce];
-    //         }
-    //     }
-    // }
+    pce = wP;
+    for(int pceNum = 0; pceNum < pos->pceNum[pce]; ++pceNum) {
+        sq = pos->pList[pce][pceNum];
+        ASSERT(SqOnBoard(sq));
+        score += PawnTable[SQ64(sq)];
+    }
 
-    // return score;
-    return 0;
+    pce = bP;
+    for(int pceNum = 0; pceNum < pos->pceNum[pce]; ++pceNum) {
+        sq = pos->pList[pce][pceNum];
+        ASSERT(SqOnBoard(sq));
+        score -= PawnTable[MIRROR64(SQ64(sq))];
+    }
 
+    pce = wN;
+    for(int pceNum = 0; pceNum < pos->pceNum[pce]; ++pceNum) {
+        sq = pos->pList[pce][pceNum];
+        ASSERT(SqOnBoard(sq));
+        score += KnightTable[SQ64(sq)];
+    }
+
+    pce = bN;
+    for(int pceNum = 0; pceNum < pos->pceNum[pce]; ++pceNum) {
+        sq = pos->pList[pce][pceNum];
+        ASSERT(SqOnBoard(sq));
+        score -= KnightTable[MIRROR64(SQ64(sq))];
+    }
+
+    pce = wB;
+    for(int pceNum = 0; pceNum < pos->pceNum[pce]; ++pceNum) {
+        sq = pos->pList[pce][pceNum];
+        ASSERT(SqOnBoard(sq));
+        score += BishopTable[SQ64(sq)];
+    }
+
+    pce = bB;
+    for(int pceNum = 0; pceNum < pos->pceNum[pce]; ++pceNum) {
+        sq = pos->pList[pce][pceNum];
+        ASSERT(SqOnBoard(sq));
+        score -= BishopTable[MIRROR64(SQ64(sq))];
+    }
+
+    pce = wR;
+    for(int pceNum = 0; pceNum < pos->pceNum[pce]; ++pceNum) {
+        sq = pos->pList[pce][pceNum];
+        ASSERT(SqOnBoard(sq));
+        score += RookTable[SQ64(sq)];
+    }
+
+    pce = bR;
+    for(int pceNum = 0; pceNum < pos->pceNum[pce]; ++pceNum) {
+        sq = pos->pList[pce][pceNum];
+        ASSERT(SqOnBoard(sq));
+        score -= RookTable[MIRROR64(SQ64(sq))];
+    }
+
+    if(pos->side == WHITE) {
+        return score;
+    } else {
+        return -score;
+    }
 }
